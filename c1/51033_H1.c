7ed4d07d04bc31d77c54ae90e6f1afe3d26a7b76
wunaozai <wunaozai@gmail.com>
2013-09-24 07:31:59 +0800

    modified fclose(fp);

--- c1/51033_H1.c
+++ c1/51033_H1.c
@@ -1,7 +1,7 @@
 /****************************************
 * Copyright (C) 2013-2013 All rights reserved.
 * @Version: 1.0
-* @Created: 2013-08-04 01:04
+* @Created: 2013-09-04 01:04
 * @Author: 林明晓 -- 563804489@qq.com
 * @Description: 
 *
@@ -45,6 +45,7 @@ int main()
 	}
     }
     fwrite(Img,256,64,fp);
+    fclose(fp);
     printf("创建%s文件成功！\n",name);
     return 0;
 }
